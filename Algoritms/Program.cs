61ecc85347d58f75b8bb0fcf83c29d467fe74d70
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-03-28 10:56:32 +0500

    Add project files.

---
+++ Algoritms/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms
+{
+    public class DailyLeetcode
+    {
+        public int SumOfEncryptedInt(int[] nums)
+        {
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                int maxDigit = 0;
+                int number = num;
+                while (number > 0)
+                {
+                    int digit = number % 10;
+                    maxDigit = Math.Max(maxDigit, digit);
+                    number /= 10;
+                }
+                int encryptedNumber = int.Parse(new string((char)(maxDigit + '0'), num.ToString().Length));
+                sum += encryptedNumber;
+            }
+            return sum;
+        }
+    }
+}
